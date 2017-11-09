b8ed7aa0a7d885fd644e37b7aff9ca82bbbc8c97
Jerry Bennett <jerrysbusiness@gmail.com>
2017-11-09 19:07:59 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,16 +1,14 @@
 # Add  code here!
 def prime?(num)
-  arr = (2..(num - 1)).to_a
+  arr = (2..(num)).to_a
   if num < 2
     false
   end
-
-    for i in arr
-      if (num % i) == 0
-        return false
-      else
-        return true
-      end
+  for i in arr
+    if (num % i) == 0
+      return false
+    else
+      return true
     end
-
+  end
 end
