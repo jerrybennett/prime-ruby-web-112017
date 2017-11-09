ef5e65c4884965f732cf37666eebce6b73bfe3c6
Jerry Bennett <jerrysbusiness@gmail.com>
2017-11-09 19:06:51 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,10 +1,10 @@
 # Add  code here!
 def prime?(num)
   arr = (2..(num - 1)).to_a
-  if num < 0
+  if num < 2
     false
   end
-  if num > 2
+
     for i in arr
       if (num % i) == 0
         return false
@@ -12,5 +12,5 @@ def prime?(num)
         return true
       end
     end
-  end
+
 end
